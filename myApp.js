6ecd2d280438fd8504acca5c397752c159469cca
AndroidMobile Device <imzee.au@gmail.com>
2024-12-28 20:09:10 +0000

    Express Server implementation

--- myApp.js
+++ myApp.js
@@ -1,39 +1,25 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+const path = require("path");
 
+// Serve Static Assets
+app.use("/public", express.static(__dirname + "/public"));
 
+console.log("Hello World");
 
+//Start a Working Express Server
+// app.get("/", function (req, res) {
+//   res.send("Hello Express");
+// });
 
+//Serve an HTML File
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+// Serve JSON on a Specific Route
+app.get("/json", function (req, res) {
+  res.json({ message: "Hello json" });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
